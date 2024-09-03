5ae90185ddd0054f63d190a6f2f835249689f1c2
DinosaurioBlue <franco21bonacina07@gmail.com>
2024-09-03 09:14:29 -0300

    first 3 functions

--- Lequel.cpp
+++ Lequel.cpp
@@ -27,20 +27,30 @@ TrigramProfile buildTrigramProfile(const Text &text)
     wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
     // Your code goes here...
-    for (auto line : text)
-    {
-        if ((line.length() > 0) &&
-            (line[line.length() - 1] == '\r'))
+    TrigramProfile TrigMap= TrigramProfile();
+    for (auto line : text){
+        if ((line.length() > 0) && (line[line.length() - 1] == '\r'))
             line = line.substr(0, line.length() - 1);
+            
+             wstring unicodeString = converter.from_bytes(line);
+
+             if (unicodeString.length() <= 3)
+        {
+            continue;
+        }
+            
+            for(int i = 0; i<unicodeString.length()-2;++i){
+                wstring temp_wstring = unicodeString.substr(i,3);//cuts the strings in 3 letters
+
+                TrigMap[converter.to_bytes(temp_wstring)]++;
+                /*It creates a key for each trigram, and increments its value if found*/
+            }
+           
+
     }
+    normalizeTrigramProfile(TrigMap);
 
-    // Tip: converts UTF-8 string to wstring
-    // wstring unicodeString = converter.from_bytes(textLine);
-
-    // Tip: convert wstring to UTF-8 string
-    // string trigram = converter.to_bytes(unicodeTrigram);
-
-    return TrigramProfile(); // Fill-in result here
+    return TrigMap;
 }
 
 /**
@@ -49,8 +59,17 @@ TrigramProfile buildTrigramProfile(const Text &text)
  * @param trigramProfile The trigram profile.
  */
 void normalizeTrigramProfile(TrigramProfile &trigramProfile)
-{
-    // Your code goes here...
+{   float temp_sum, temp_sqr;
+    temp_sqr = temp_sum = 0.0F;
+    for(auto &it : trigramProfile){//iterating each key
+        temp_sqr = (it.second*it.second);
+        temp_sum += temp_sqr;
+    }  
+    temp_sum= sqrt(temp_sum);//norm of the vector 
+    for(auto &it : trigramProfile){//iterating each key
+        it.second /= temp_sum;//normalizing each vector 
+    }  
+
 
     return;
 }
@@ -64,9 +83,14 @@ void normalizeTrigramProfile(TrigramProfile &trigramProfile)
  */
 float getCosineSimilarity(TrigramProfile &textProfile, TrigramProfile &languageProfile)
 {
-    // Your code goes here...
-
-    return 0; // Fill-in result here
+    float cos_sim;
+    for(auto &element : textProfile ){
+        auto temp = languageProfile.find(element.first);
+        if(temp != languageProfile.end()){
+            cos_sim+=(temp->second * element.second); 
+        }
+    }
+    return cos_sim;
 }
 
 /**
@@ -78,7 +102,7 @@ float getCosineSimilarity(TrigramProfile &textProfile, TrigramProfile &languageP
  */
 string identifyLanguage(const Text &text, LanguageProfiles &languages)
 {
-    // Your code goes here...
+    
 
     return ""; // Fill-in result here
 }
